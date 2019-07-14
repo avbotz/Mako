b2fb83823bf21a766608fd479b51d70b529ee081
David Zhang <davidzhang501@gmail.com>
2019-07-14 14:41:04 -0700

    Refactoring almost done, needs testing

--- src/sub_mission/src/client.cpp
+++ src/sub_mission/src/client.cpp
@@ -1,5 +1,6 @@
 #include "mission/client.hpp"
 
+
 namespace vision_client 
 {
 	void setTask(Task task)
@@ -15,7 +16,8 @@ namespace vision_client
 	Observation vision()
 	{
 		client.call(srv);
-		Observation 
+		return Observation(srv.response.prob, srv.response.r, srv.response.c, 
+				srv.response.hangle, srv.response.vangle);
 	}
 };
 
@@ -23,21 +25,38 @@ namespace control_client
 {
 	bool alive()
 	{
-
+		control::ControlAlive::Request req;
+		control::ControlAlive::Response rep;
+		alive_client.call(req, rep);
+		return rep.data;
 	}
 
 	State state()
 	{
-
+		control::ControlState::Request req;
+		control::ControlState::Response rep;
+		state_client.call(req, rep);
+		return State(rep.F, rep.H, rep.V, rep.Y, rep.P, rep.R);
 	}
 
-	void write(std::string) 
+	void write(std::string input)
 	{
-
+		control::ControlWrite::Request req;
+		control::ControlWrite::Response rep;
+		req.data = input;
+		write_client.call(req, rep);
 	}
 
-	void writeState(const State &) 
+	void writeState(const State &state)
 	{
-
+		control::ControlWriteState::Request req;
+		control::ControlWriteState::Response rep;
+		req.F = state.axis[X];
+		req.H = state.axis[Y];
+		req.V = state.axis[Z];
+		req.Y = state.axis[YAW];
+		req.P = state.axis[PITCH];
+		req.R = state.axis[ROLL];
+		write_state_client.call(req, rep);
 	}
 };
--- src/sub_vision/src/service.cpp
+++ src/sub_vision/src/service.cpp
@@ -1,7 +1,30 @@
-#include "vision/service.hpp"
-#include "vision/tasks.hpp"
+#include <ros/ros.h>
 #include "vision/config.hpp"
+#include "vision/service.hpp"
+#include "vision/log.hpp"
 
+
+void VisionService::captureCallback(const sensor_msgs::ImageConstPtr &msg)
+{
+	// Read front camera data from ROS Spinnaker publisher.
+	try 
+	{
+		cv::Mat image = cv_bridge::toCvShare(msg, "bgr8")->image;
+		image.copyTo(this->front);
+		if (LOG) 
+		{
+			log(this->front, 'f');
+		}
+		else 
+		{
+			// ROS_INFO("Received image from acquisition.");
+		}
+	}
+	catch (cv_bridge::Exception &e)
+	{
+		ROS_ERROR("Could not read image from Spinnaker publisher.");
+	}
+}
 
 bool VisionService::detectCallback(vision::Vision::Request &req, 
 		vision::Vision::Response &res)
